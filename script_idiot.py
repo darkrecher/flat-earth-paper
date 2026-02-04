3e513dbae7da0fbb4b5b47f53dcca760a778b51b
Réchèr <recher_burp@yahoo.fr>
2026-02-04 17:23:01 +0100

    Add files via upload

--- script_idiot.py
+++ script_idiot.py
@@ -1,15 +1,28 @@
 XOR_KEY = b"obfuscation de la choucrouterie ultime !! 834513259765410 TONNERRE DE CHABRAQUE!!"
 
-def main():
-    bin_data = open("graphql_server.py", "rb").read()
+def encode_one_file(filename_in, filename_out):
+    bin_data = open(filename_in, "rb").read()
     mul_key = len(bin_data) // len(XOR_KEY) + 1
     xored_ints = [
         f"{a^b:02x}" for a, b in zip(bin_data, XOR_KEY * mul_key)
     ]
-    file_out = open("graphql_server_enc.txt", "w")
+    file_out = open(filename_out, "w")
     file_out.write("".join(xored_ints))
     file_out.close()
 
 
+def main():
+    encode_one_file("graphql_server.py", "graphql_server_enc.txt")
+    encode_one_file("papers_fake_db.py", "papers_fake_db_enc.txt")
+
+
 if __name__ == "__main__":
     main()
+
+"""
+
+curl -X POST https://flat-earth-paper.onrender.com/graphql -H "Content-Type: application/json"   -d '{"query": "{ article(articleId: \"paper_001\") { title issuerId keywords } }"}'
+
+curl -X POST http://localhost:3000/graphql -H "Content-Type: application/json"   -d '{"query": "{ article(articleId: \"paper_001\") { title issuerId keywords } }"}'
+
+"""
