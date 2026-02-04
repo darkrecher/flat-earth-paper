0f4caf7a24f411344db7868e91ee5d8bd54a6b60
Réchèr <recher_burp@yahoo.fr>
2026-02-04 14:31:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from typing import Optional
 
+import strawberry
+from strawberry.fastapi import GraphQLRouter
+from strawberry.extensions import SchemaExtension
+from graphql import GraphQLError
+
+
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,7 +14,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World all is well for now"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
